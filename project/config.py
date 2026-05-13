5264f39349685213c57bc294bad7e357c3fda53e
chunyu zheng <130476780+chunyuzheng152@users.noreply.github.com>
2026-05-13 22:37:19 +0800

    Update project/config.py
    Co-authored-by: Cameron Wright <145316390+cameronwright62@users.noreply.github.com>

--- project/config.py
+++ project/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 default_db_path = "sqlite:///" + os.path.join(basedir, "app.db")
 
 class Config:
-    SECRET_KEY = os.environ.get("APP_SECRET_KEY") or "dev-secret-key"
+    SECRET_KEY = os.environ.get("APP_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("APP_DATABASE_URL") or default_db_path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
